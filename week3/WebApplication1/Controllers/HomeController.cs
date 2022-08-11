fb2952bb665d2108edb169c2cea268822774964a
GreenH <huangshixin47@gmail.com>
2022-08-11 12:07:30 +1000

    week3

---
+++ week3/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.HelloWorld;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            //  created an instance of the Hello object
+
+            Hello hello = new Hello();
+
+            //  assigned the ViewBag.Message to the result
+            ViewBag.Message = hello.GetHello();
+
+            ExampleDictionary ed = new ExampleDictionary();
+
+            ed.Example();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ week3/WebApplication1/Models/Exercise/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models.Exercise;
+
+namespace WebApplication1.Models.Exercise
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public Student(String name, String phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
+
+
+public class ExampleDictionary
+{
+    public void Example()
+    {
+        Dictionary<int, Student> studentDictionary = new Dictionary<int,
+        Student>();
+        Student s1 = new Student("Russel", "0423456789");
+        Student s2 = new Student("Jian", "0456789123");
+        studentDictionary.Add(1, s1);
+        studentDictionary.Add(2, s2);
+        Student result = new Student("", "");
+        studentDictionary.TryGetValue(1, out result);
+    }
+}
