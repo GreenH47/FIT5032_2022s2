d6d1cb5ffed393c4daee6e3599408d37414d7211
GreenH <huangshixin47@gmail.com>
2022-10-09 19:08:47 +1100

    week9

---
+++ week9/CoreAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
